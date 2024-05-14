3f7ad15e890962516cde44675d4989ab10130c89
Erielle Joeshua Bancolita <eriellebancolita017@gmail.com>
2024-05-14 19:45:43 +0800

    :children_crossing: update identifying operation

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -10,31 +10,46 @@ class CalculatorController extends Controller
     {
         $input = $request->input('expression');
         $result = null;
+        $operator = null;
 
-        // Parse the input expression
-        $tokens = explode(' ', $input);
-        $operator = $tokens[1] ?? null;
+        // Check if the input contains a valid operator
+        if (strpos($input, '+') !== false) {
+            $operator = '+';
+        } elseif (strpos($input, '-') !== false) {
+            $operator = '-';
+        } elseif (strpos($input, '*') !== false) {
+            $operator = '*';
+        } elseif (strpos($input, '/') !== false) {
+            $operator = '/';
+        } elseif (strpos($input, 'sqrt') !== false) {
+            $operator = 'sqrt';
+        }
 
         // Perform the corresponding operation
-        switch ($operator) {
-            case '+':
-                $result = $tokens[0] + $tokens[2];
-                break;
-            case '-':
-                $result = $tokens[0] - $tokens[2];
-                break;
-            case '*':
-                $result = $tokens[0] * $tokens[2];
-                break;
-            case '/':
-                $result = $tokens[0] / $tokens[2];
-                break;
-            case 'sqrt':
-                $result = sqrt($tokens[0]);
-                break;
-            default:
-                $result = 'Invalid operator';
-                break;
+        if ($operator === 'sqrt') {
+            $number = (float) str_replace('sqrt', '', $input);
+            $result = sqrt($number);
+        } elseif ($operator) {
+            $operands = explode($operator, $input);
+            $operand1 = (float) $operands[0];
+            $operand2 = (float) $operands[1];
+
+            switch ($operator) {
+                case '+':
+                    $result = $operand1 + $operand2;
+                    break;
+                case '-':
+                    $result = $operand1 - $operand2;
+                    break;
+                case '*':
+                    $result = $operand1 * $operand2;
+                    break;
+                case '/':
+                    $result = $operand1 / $operand2;
+                    break;
+            }
+        } else {
+            $result = 'Invalid operator';
         }
 
         return response()->json([
